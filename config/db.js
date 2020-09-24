0a5544ba62404e58c4395a97f31a69efe5e6b3ae
Amit Kumar <aksingh1109@gmail.com>
2020-09-24 10:05:46 +0530

    Version (1.0.0)

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require ('mongoose');
+const config = require('config');
+const db = config.get('mongo URI');
+const connectDB = async() => {
+    try{
+        await mongoose.connect(db,
+                     {useNewUrlParser: true}
+            );
+        console.log('mongo DB connected ---');
+    }
+    catch(err){
+        console.error(err.message); //Exit process with failure
+        process.exit(1);
+    }
+};
+module.exports =connectDB; 
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const connectDB =require('./config/db');
+
 const app = express();
+//connect database
+connectDB();
 
 app.get('/',(req,res)=>res.send('API Running'));
 
 const PORT =process.env.PORT||5000;
 
 app.listen(PORT,() => 
-console.log('server is running on port ${PORT}')
+console.log(`server is running on port ${PORT}`)
 );
