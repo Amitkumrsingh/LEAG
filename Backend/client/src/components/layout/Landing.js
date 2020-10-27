9a840ccc7c246e106a74e9a0495adcd0964f5325
Amit Kumar <aksingh1109@gmail.com>
2020-10-27 20:08:14 +0530

    Version(1.0.0)

---
+++ Backend/client/src/components/layout/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+const Landing = () => {
+    return (
+        <section className="landing">
+        <div className="dark-overlay">
+          <div className="landing-inner">
+            <h1 className="x-large">Learn English And Grow</h1>
+            <p className="lead">
+              
+            </p>
+            <div className="buttons">
+              <Link to="/register" className="btn btn-primary">Sign Up</Link>
+              <Link to="/login" className="btn btn-light">Login</Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+}
+
+export default Landing; 
