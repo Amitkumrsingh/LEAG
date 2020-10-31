f1aed47ab45f13412f5fd6d04de3be330c1fbf53
Amit Kumar <aksingh1109@gmail.com>
2020-10-31 12:00:33 +0530

    added files for deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,28 +1,32 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const connectDB = require('./config/db');
 
+const path = require('path');
 
 const app = express();
 
 //connect database
 connectDB();
 
-//init Middleware
+// Init Middleware
+app.use(express.json());
 
-app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('Hello World'));
-
-// Use Routes
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
